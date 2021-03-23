1fd6c8f30644304b9c1cb0325d6f0d6e87232857
Davide Andreana <davin874@gmail.com>
2021-03-23 13:31:36 +0100

    Fine lezione

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -29,6 +29,11 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHeroById(id).subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
   goBack(): void {
     this.location.back();
   }
